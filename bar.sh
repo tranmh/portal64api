0ced2f3ee7f31ff0e44c1fdc889cd51969692322
Minh Cuong Tran <minh-cuong.tran@schulergroup.com>
2025-07-27 23:22:53 +0200

    shortcut for compile and run

---
+++ bar.sh
@@ -0,0 +1,15 @@
+/root/go/bin/swag init -g cmd/server/main.go -o docs/generated
+make build
+export MVDSB_USERNAME=portal
+export MVDSB_PASSWORD='Usm@1?/#Qv^avF'
+export MVDSB_HOST=localhost
+export MVDSB_DATABASE=mvdsb
+export PORTAL64_BDW_USERNAME=portal
+export PORTAL64_BDW_PASSWORD='Usm@1?/#Qv^avF'
+export PORTAL64_BDW_HOST=localhost
+export PORTAL64_BDW_DATABASE=portal64_bdw
+export PORTAL64_SVW_USERNAME=portal
+export PORTAL64_SVW_PASSWORD='Usm@1?/#Qv^avF'
+export PORTAL64_SVW_HOST=localhost
+export PORTAL64_SVW_DATABASE=portal64_svw
+./bin/portal64api
